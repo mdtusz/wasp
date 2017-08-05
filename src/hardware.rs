079ec9e6933b0b34a1d41ff44c3663882296d72e
Tim <timothyhollabaugh@gmail.com>
2017-08-05 16:44:44 -0400

    attempt

--- src/hardware.rs
+++ src/hardware.rs
@@ -1,38 +1,16 @@
 
-#[derive(Debug, Clone, Copy, PartialEq)]
-pub enum PinState {
-    High,
-    Low
+// Pin posibilities
+
+// enum Pin { Input(InputPin), Output(OutputPin) }
+
+// trait Pin {}
+// trait OutputPin : Pin {}
+// trait InputPin : Pin{}
+
+trait Pin {
+
 }
 
-#[derive(Debug, Clone, Copy, PartialEq)]
-pub enum PinMode {
-    Input,
-    Output,
-    InputPullup,
-    InputPulldown,
-    OutputOpenDrain
-}
-
-/// Anything that can read and write GPIO pins in hardware
-/// Pin: The type for a pin number
-/// Mode: The type representing the mode of a pin
-pub trait HardwareGpio {
-    fn pin_mode(&mut self, pin: u8, mode: PinMode);
-    fn digital_write(&mut self, pin: u8, val: PinState);
-    fn digital_read(&self, pin: u8) -> PinState;
-}
-
-/// Anything that can provide a hardware Uart to talk to a host
-pub trait HardwareUart {
-    fn readable(&self) -> bool;
-    fn try_read_byte(&self) -> Result<u8, &'static str>;
-    fn write_bytes(&self, bytes: &[u8]) -> Result<(), ()>;
-}
-
-/// Anything that can provide the time since start and delay
-/// All units are microseconds
-pub trait HardwareTime {
-    fn delay(&self, micros: u32);
-    fn now(&self) -> u32;
+trait DigitalInputPin {
+    
 }
